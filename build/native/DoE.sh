40aa90add6f89827205d80e0e3d26ea2541766d1
vsgirelli <vsgirelli@inf.ufrgs.br>
2020-08-21 14:10:34 -0300

    Adding scripts for execs with turbo boost off and all OMP_WAIT_POLICY

--- build/native/DoE.sh
+++ build/native/DoE.sh
@@ -13,24 +13,24 @@ else
 	OUTPUT=./DoE/`hostname | awk -F. {'print $1'}`.nas.csv
 fi
 
+declare -a SIZE=("W" "A")
 declare -a THREADS=("1" "2" "4" "8" "12")
-declare -a METRICS=("cycles" "instructions" "l2_rqsts.demand_data_rd_hit" "l2_rqsts.demand_data_rd_miss" "l2_rqsts.all_pf" "l2_rqsts.pf_hit" "l2_rqsts.pf_miss" "l2_lines_out.useless_hwpf" "l2_lines_out.useless_pref" "offcore_response.pf_l2_data_rd.any_response" "offcore_response.pf_l2_data_rd.l3_hit.any_snoop" "offcore_response.pf_l2_data_rd.l3_hit.hitm_other_core" "offcore_response.pf_l2_rfo.any_response" "offcore_response.pf_l2_rfo.l3_hit.any_snoop" "offcore_response.pf_l2_rfo.l3_hit.hitm_other_core")
-
+declare -a METRICS=("cycles" "instructions" "l2_rqsts.demand_data_rd_hit" "l2_rqsts.demand_data_rd_miss" "l2_rqsts.all_pf" "l2_rqsts.pf_hit" "l2_rqsts.pf_miss" "l2_lines_out.useless_hwpf" "l2_lines_out.useless_pref" "offcore_response.pf_l2_data_rd.any_response" "offcore_response.pf_l2_data_rd.l3_hit.any_snoop" "offcore_response.pf_l2_data_rd.l3_hit.hitm_other_core" "offcore_response.pf_l2_rfo.any_response" "offcore_response.pf_l2_rfo.l3_hit.any_snoop" "offcore_response.pf_l2_rfo.l3_hit.hitm_other_core" "offcore_requests.demand_data_rd" "offcore_requests.all_data_rd")
 
 
 rm -f $OUTPUT
 
-SIZE=0
+
 for application in "${APP[@]}"; do
 	for cpus in "${THREADS[@]}"; do
-		for event in "${METRICS[@]}"; do
-			echo "$application;$cpus;$event" >> $OUTPUT
-			size=$((size+1))
+    for event in "${METRICS[@]}"; do
+      for size in "${SIZE[@]}"; do
+        echo "$application;$cpus;$event;$size" >> $OUTPUT
+      done
 		done
 	done
 done
 
-printf "\tcreating full factorial with $size runs ...\n"
 
 for i in `seq 1 10`; do
 	shuf $OUTPUT -o $OUTPUT
