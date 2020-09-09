9970bf385deceb5014c3efe25803dbd3f06dd3c6
vsgirelli <vsgirelli@inf.ufrgs.br>
2020-09-09 16:30:38 -0300

    Adding script and DoE for rodinia (for now backprop only)

---
+++ build/native/DoE_rod.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage : $0 <benchmark>"
+  exit 1
+fi
+
+mkdir -p DoE_rod
+
+if [ "$1" == "spec" ]; then
+  declare -a APP=("applu331" "botsalgn" "botsspar" "bt331" "bwaves" "fma3d" "ilbdc" "imagick" "kdtree" "md" "mgrid331" "nab" "smithwa" "swim")
+  OUTPUT=./DoE/`hostname | awk -F. {'print $1'}`.spec.csv
+else
+  if [ "$1" == "nas" ]; then
+    #declare -a APP=("bt" "cg" "ep" "ft" "is" "lu" "mg" "sp" "ua")
+    declare -a APP=("cg")
+    OUTPUT=./DoE/`hostname | awk -F. {'print $1'}`.nas.csv
+  else
+    declare -a APP=("backprop")
+    OUTPUT=./DoE_rod/`hostname | awk -F. {'print $1'}`.rod.csv
+  fi
+  fi
+
+#declare -a SIZE=("W" "A" "B")
+declare -a SIZE=("1048576" "2097152" "4194304" "8388608" "16777216" "33554432" "67108864" "134217728" "268435456")
+declare -a THREADS=("1" "2" "4" "8" "12")
+declare -a METRICS=("PAPI_TOT_INS" "PAPI_TOT_CYC" "PAPI_TOT_CYC" "PAPI_TOT_INS" "L2_RQSTS:DEMAND_DATA_RD_HIT" "L2_RQSTS:DEMAND_DATA_RD_MISS" "L2_RQSTS:ALL_PF" "L2_RQSTS:PF_HIT" "L2_RQSTS:PF_MISS" "L2_LINES_OUT:USELESS_HWPREF" "L2_LINES_OUT:USELESS_HWPF" "OFFCORE_RESPONSE_0:PF_L2_DATA_RD:ANY_RESPONSE" "OFFCORE_RESPONSE_0:PF_L2_DATA_RD:L3_HIT:SNP_ANY" "OFFCORE_RESPONSE_0:PF_L2_DATA_RD:L3_HIT:SNP_HITM" "OFFCORE_RESPONSE_0:PF_L2_RFO:ANY_RESPONSE" "OFFCORE_RESPONSE_0:PF_L2_RFO:L3_HIT:SNP_ANY" "OFFCORE_RESPONSE_0:PF_L2_RFO:L3_HIT:SNP_HITM" "OFFCORE_REQUESTS:DEMAND_DATA_RD" "OFFCORE_REQUESTS:ALL_DATA_RD" "OFFCORE_REQUESTS.L3_MISS_DEMAND_DATA_RD")
+#declare -a METRICS=("MEM_LOAD_L3_HIT_RETIRED.XSNP_MISS" "MEM_LOAD_L3_HIT_RETIRED.XSNP_HIT" "MEM_LOAD_L3_HIT_RETIRED.XSNP_HITM" "MEM_LOAD_L3_HIT_RETIRED.XSNP_NONE")
+
+rm -f $OUTPUT
+
+for application in "${APP[@]}"; do
+  for cpus in "${THREADS[@]}"; do
+    for event in "${METRICS[@]}"; do
+      for size in "${SIZE[@]}"; do
+        echo "$application;$cpus;$event;$size" >> $OUTPUT
+      done
+    done
+  done
+done
+
+
+for i in `seq 1 30`; do
+  shuf $OUTPUT -o $OUTPUT
+done
